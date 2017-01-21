2487abd920187d22ed023401a9d9c0adc5c001c3
Patryk Kisielewski <patryk.kisielewski@wp.eu>
2017-01-21 14:17:09 +0100

    add check apikey function

---
+++ lib/apikeys.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+import 'package:chatbot_server/global.dart';
+import 'package:chatbot_server/formating.dart';
+
+List checkApiKey(String apikey){
+	bool is_apikey = false;
+	String address = '';
+	var list  = apikeys.readAsLinesSync();
+	for(var i = 0; i < list.length; i++){
+		if(list[i].split(";")[0] == apikey){
+			is_apikey = true;
+			address = list[i].split(";")[1];
+			break;
+		}
+	}
+	return [ is_apikey, address];
+}
