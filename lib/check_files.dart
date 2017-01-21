47c13d2899f96552aff930cb0302c97618db856f
Patryk Kisielewski <patryk.kisielewski@wp.eu>
2017-01-21 03:34:21 +0100

    check files

---
+++ lib/check_files.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+import 'package:chatbot_server/global.dart';
+import 'package:chatbot_server/formating.dart';
+import 'package:chatbot_server/logfile.dart';
+
+bool checkFiles(){
+	var logs = new Directory('logs');
+	logs.createSync(recursive: true);
+	if(logs.existsSync() == false){
+		print("Error: Directory 'logs' not created");
+		return false;
+	}	
+	logfile = new File(logfileName());
+	logfile.createSync(recursive: true);
+	if(logfile.existsSync()){
+		print("Log file created");
+	} else {
+		print("Error: Log file not created");
+		return false;
+	}
+	apikeys.createSync(recursive: true);
+	if(apikeys.existsSync() == false){
+		log("Error: ApiKeys file not created");
+		return false;
+	}
+	if(database.existsSync() == false){
+		database.createSync(recursive: true);
+	}
+	if(database.existsSync() == false){
+		log("Error: Database file not created");
+		return false;
+	}
+	log("Check files done");
+}
