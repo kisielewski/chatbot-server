f50fd290996b2779cdd1ba4da7468ad04449bb6f
Patryk Kisielewski <patryk.kisielewski@wp.eu>
2017-01-21 16:17:52 +0100

    add insertAnswer and init selectAnswer

--- lib/database.dart
+++ lib/database.dart
@@ -5,3 +5,11 @@ import 'package:chatbot_server/global.dart';
 void firstRecord(){
 	database.writeAsStringSync("Who are you?;I'm ChatBot.;v"+'\r\n', mode: FileMode.APPEND);
 }
+
+void insertAnswer(String key, String val){
+	database.writeAsStringSync(key+';'+val+';u'+'\r\n', mode: FileMode.APPEND);
+}
+
+String selectAnswer(String data){
+	return "Dziala";
+}
