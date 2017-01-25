351c5ec2456514513bc32cdf5e59b80f15552c13
Patryk Kisielewski <patryk.kisielewski@wp.eu>
2017-01-25 22:52:59 +0100

    log change separtor '|' instead ';'

--- lib/log.dart
+++ lib/log.dart
@@ -5,15 +5,15 @@ import 'package:chatbot_server/formating.dart';
 File logfile = new File(logfileName());
 
 void log(String data) {
-	logfile.writeAsStringSync(currentDate()+';'+data+'\r\n', mode: FileMode.APPEND);
+	logfile.writeAsStringSync(currentDate()+'|'+data+'\r\n', mode: FileMode.APPEND);
 	print(data);
 }
 
 void logAnswer(HttpRequest request){
 	String data = "ask;";
 	if(request.uri.queryParameters.containsKey('userquestion')) data += request.uri.queryParameters['userquestion'];
-	data += ';';
+	data += '|';
 	if(request.uri.queryParameters.containsKey('useranswer')) data += request.uri.queryParameters['useranswer'];
-	data += ';'+request.headers.value('X-Forwarded-For');
+	data += '|'+request.headers.value('X-Forwarded-For');
 	log(data);
 }
