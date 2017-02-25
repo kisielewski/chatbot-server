8e778c4b6c180fd5c53dcd37b20e45a288231f3f
Patryk Kisielewski <patryk.kisielewski@wp.eu>
2017-02-25 11:07:15 +0100

    admin server

--- bin/main.dart
+++ bin/main.dart
@@ -1,10 +1,12 @@
 import 'dart:io';
 import 'package:chatbot_server/check_files.dart';
 import 'package:chatbot_server/http_server.dart';
+import 'package:chatbot_server/http_admin_server.dart';
 
 void main() {
 	if(checkFiles() == false){
 		exit(2);
 	}
 	runServer();
+	runAdminServer();
 }
---
+++ lib/http_admin_server.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+import 'package:chatbot_server/log.dart';
+//import 'package:chatbot_server/http_response.dart';
+
+HttpServer admin_server;
+const ADMIN_PORT = 4042;
+
+runAdminServer() async {
+	admin_server = await HttpServer.bind(InternetAddress.ANY_IP_V4, ADMIN_PORT);
+	log("admin_server|start|"+ADMIN_PORT.toString());
+	await for (HttpRequest request in admin_server) {
+		print(request.method);
+		print(request.toString());
+	}
+}
